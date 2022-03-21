4be14447b6ba5faa9736ce86f1d9e164ab725015
Laurier Rose <laurence.bellebouche@edu.esiee-it.fr>
2022-03-21 12:12:13 +0100

    search_scene.js component created

---
+++ components/search_scene.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class search_scene extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> search_scene </Text>
+      </View>
+    );
+  }
+}
+
+export default search_scene;
