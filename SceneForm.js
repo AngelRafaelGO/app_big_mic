7bf8ab63676be1b40a1fba25a0e07409e9cf1c74
Sofie <92948674+sofiefrance@users.noreply.github.com>
2022-03-28 09:40:06 +0200

    formular improvements
    mise en place des setters.

--- SceneForm.js
+++ SceneForm.js
@@ -1,184 +1,124 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Image, TextInput,ScrollView, Button } from 'react-native';
+import SceneCard from './SceneCard';
+
 
 
 const SceneForm = () => {
 
-    React.useState
+    const [sceneTitle, setSceneTitle] = useState('');
+    const [sceneAdress, setSceneAdress] = useState('');
+    const [sceneDescription, setSceneDescription] = useState('');
+    const [sceneTags, setSceneTags] = useState('');
+    const [sceneRequirement, setSceneRequirement] = useState('');
+    const [sceneDate, setSceneDate] = useState('');
+    const [sceneImage, setSceneImage] = useState('');
+
    
-
-    // handletitreScene = (e) => {
-    //     this.setState(e.target.value)
-
-    // }
-
-    // handleDescription = (e) => {
-    //     this.setState(e.target.value)
-    // }
-
-    let text = useState();
-
-
     return (
 
         <ScrollView> 
 
-            <Text>Creation de ma scène </Text>
+            <Text> Création de ma scène  </Text>
             
-            <View styles={styles.wrapper}>
+            <View styles={styles.container}>
+                
 
-            <Text> Titre de la scène : </Text>
-            <TextInput style={styles.TextInput}
-                        
+                    <Text styles={styles.textOne}> Titre de la scène : </Text>
+                    <TextInput style={styles.textInput}
+                                onChangeText={ (val) => setSceneTitle(val)} />
+                                
 
-            />
+                    <Text styles={styles.textOne}> Adresse de la scène :</Text>
+                    <TextInput style={styles.textInput}
+                                onChangeText={ (val) => setSceneAdress(val)} />
 
-            <Text> Adresse de la scène :</Text>
-            <TextInput style={styles.TextInput}
-                        
+                    <Text styles={styles.textOne}> Description :</Text>
+                    <TextInput style={styles.textInputDescription} multiline={true}
+                                onChangeText={ (val) => setSceneDescription(val)} />
 
-            />
+                    <Text styles={styles.textOne}> Tags :</Text>
+                    <TextInput style={styles.textInput}
+                                onChangeText={ (val) => setSceneTags(val)} />
 
-            <Text> Description :</Text>
-            <TextInput style={styles.TextInput}
-                        
+                    <Text styles={styles.textOne}> Critères de participation :</Text>
+                    <TextInput style={styles.textInput}
+                                multiline
+                                onChangeText={ (val) => setSceneRequirement(val)} />
 
-            />
+                    <Text styles={styles.textOne}> Dates de la scène</Text>
+                    <TextInput style={styles.textInput}
+                                onChangeText={ (val) => setSceneDate(val)} 
+                                keyboardType='numeric' />
 
-            <Text> Tags :</Text>
-            <TextInput style={styles.TextInput}
-                        
+                    <Text styles={styles.textOne}> Charger une photo pour la scène :</Text>
 
-            />
+                    <Button title="Valider" color='red' borderRadius= '50' fontWeight="bold" padding='20'  
+                    onPress= {()=>
+                        {
+                            if( sceneTitle == '' ||  sceneAdress == '' ||  sceneDescription == '' || sceneTags== '' || sceneRequirement == '' || sceneDate== '' ) {
+                                alert('Vous n\'avez pas rentré toutes les données')
+                            }
 
-            <Text> Critères de participation :</Text>
-            <TextInput style={styles.TextInput}
-                        
+                            // else {
+                            //     return <SceneCard/>
 
-            />
+                            // }
+                        }
 
-            <Text> Charger une photo pour la scène :</Text>
+                    }
 
-            
+                    />       
 
- 
             </View>
-
-            <View >
-                <Button title="Valider" StyleSheet={styles.Button} />
-
-            </View>
-
             
-
         </ScrollView>
     )
-
 } 
  
 const styles = StyleSheet.create({
 
-    
-    Button: {
-        backgroundColor: "#ff0000",
-        fontSize: 40,
-    },
-
-    wrapper: {
-        marginTop: 50,
-        height: 300,
+    container: { 
+        flex:1,
         alignItems:"center",
+        padding:20,
+        margin:10,
+        marginTop: 10,
+        height: 100,  
     },
 
-    TextInput: { 
+    textOne: {
+        fontWeight: 'bold',   
+    },
 
-        height: 30,
+    textInput: {
+        
+        backgroundColor:'#D0D0D0',
+        borderRadius: 10,
+        height: 32,
         width: '90%',
-        borderColor: 'grey',
+        borderColor: '#B0B0B0',
+        borderWidth: 1,
+        padding: 5,
+        margin:15,
+    },
+
+    textInputDescription: {
+        backgroundColor:'#D0D0D0',
+        borderRadius: 10,
+        height: 100,
+        width: '90%',
+        borderColor: '#B0B0B0',
         borderWidth: 1,
         padding: 5,
         margin:15,
     }
+ 
 });
 
 export default SceneForm;
+    
+        
 
-   {/* <Text> Titre de la scène </Text>
-            <TextInput style={{height:40, borderColor: 'gray', borderWidth:1}} value={this.state.titreScene} onChange = {this.handletitreScene} ></TextInput>
-
-            <View>
-                <Text> Date :</Text>
-
-                <Select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
-
-                </Select>
-
-            </View>
-
-            <View>
-                <Text> Mois :</Text>
-
-                <Select>
-                    <option value="Janvier">Janvier</option>
-                    <option value="Fevrier">Février</option>
-                    <option value="Mars">Mars</option>
-                    <option value="Avril">Avril</option>
-                    <option value="Mai">Mai</option>
-                    <option value="Juin">Juin</option>
-                    <option value="Juillet">Juillet</option>
-                    <option value="Aout">Août</option>
-                    <option value="Septembre">Septembre</option>
-                    <option value="Octobre">Octobre</option>
-                    <option value="Novembre">Novembre</option>
-                    <option value="Decembre">Decembre</option>
-
-                </Select>
-
-            </View>
-
-            <View>
-                <Text> Date :</Text>
-
-                <Select>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                    
-
-                </Select>
-
-            </View>
-
-            <Text> Description :</Text>
-            <TextInput style={{height:40, borderColor: 'gray', borderWidth:1}} value={this.state.description} onChange={this.handleDescription}></TextInput> */}
+    
